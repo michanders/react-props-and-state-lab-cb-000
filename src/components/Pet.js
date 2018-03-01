c34b99f5235f228ab143e78f02898786c430f83a
Michael Anderson <michanders@msn.com>
2018-03-01 02:24:35 +0000

    automatically backed up by learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -18,8 +18,8 @@ class Pet extends React.Component {
             <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
